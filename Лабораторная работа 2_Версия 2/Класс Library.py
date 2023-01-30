90a944f5f8d0c46e29bf492f5e84bbe820a8846c
Kseniya-Onikiichuk <119221191+Kseniya-Onikiichuk@users.noreply.github.com>
2023-01-30 21:34:32 +0300

    ООП_Лабораторная работа 2_Версия 2

---
+++ Лабораторная работа 2_Версия 2/Класс Library.py
@@ -0,0 +1,97 @@
+from typing import Optional
+
+
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+class Book:
+    def __init__(self, id_: int, name: Optional[str], pages: Optional[int]):
+        """
+        Создание объекта "Книга"
+
+        :param id_: Идентификатор книги
+        :param name: Название книги
+        :param pages: Количество страниц в книге
+        """
+        if not isinstance(id_, int):
+            raise TypeError("Идентификатор книги должен быть типа int")
+        self.id = id_
+
+        if not isinstance(name, str):
+            raise TypeError("Название книги должно быть типа str")
+        self.name = name
+
+        if not isinstance(pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f'Book(id_={self.id!r}, name={self.name!r}, pages={self.pages!r})'
+
+
+class Library:
+    def __init__(self, books: Optional[list] = []):
+        """
+        Создание объекта "Библиотека"
+
+        :param books: Список книг
+        """
+        if not isinstance(books, list):
+            raise TypeError("Список книг должен быть list")
+        self.books = books
+
+    def get_next_book_id(self) -> int:
+        """
+        Метод, возвращающий идентификатор для добавления новой книги в библиотеку
+
+        :return: Если в библиотеке нет книг, то 1. Если книги есть, то индентификатор последней книги, увеличенный на 1.
+        """
+        if self.books == []:
+            return 1
+        else:
+            last_book = self.books[-1]
+            return last_book.__dict__['id'] + 1
+
+    def get_index_by_book_id(self, index: int) -> int:
+        """
+        Метод, возвращающий индекс книги в списке, который хранится в атрибуте экземпляра класса
+
+        :return: Индекс книги из списка
+        """
+        if not isinstance(index, int):
+            raise TypeError("Индекс книги должен быть int")
+        self.index = index
+
+        for i, value in enumerate(self.books):
+            if value.__dict__['id'] == index:
+                return i
+        raise ValueError("Книги с запрашиваемым id не существует")
+
+
+if __name__ == '__main__':
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
